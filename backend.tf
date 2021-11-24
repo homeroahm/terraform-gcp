e01d94b1f20204817400a767d744d2f60bba848f
Antonio Machado <homeroahm@hotmail.com>
2021-11-23 21:41:16 -0500

    executed tform fmt

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
 #Store State at Terraform Cloud
 
 terraform {
-    backend "remote" {
-        hostname = "app.terraform.io"
-        organization = "terraformers21"
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "terraformers21"
 
-        workspaces {
-           name = "gke-infra2"
-        }
+    workspaces {
+      name = "gke-infra2"
     }
+  }
 }
